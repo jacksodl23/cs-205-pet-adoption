5a878f2da6ece1e39d5d9a23de1aab7dbc425c4e
John Pitkin <jlp039@bucknell.edu>
2021-04-06 13:11:58 -0400

    updated welcome page

--- PetProjectGUI/mainwindow.cpp
+++ PetProjectGUI/mainwindow.cpp
@@ -1,11 +1,31 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+void MainWindow::setWelcomePhoto() {
+    int numberOfPhotos = 3;
+
+    srand(time(0));
+    int photoNum = rand() % numberOfPhotos;
+
+    QString photoString = QString::number(photoNum);
+
+    QString tempPath("../../../../CoverPhoto");
+    tempPath.append(photoString);
+    QString filePath = tempPath.append(".jpg");
+    qDebug() << filePath;
+
+    welcomePic.load(filePath);
+    int width = ui->labelPic->width();
+    int height = ui->labelPic->height();
+    ui->labelPic->setPixmap(welcomePic.scaled(width, height, Qt::KeepAspectRatio));
+}
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWelcomePhoto();
 }
 
 MainWindow::~MainWindow()
--- PetProjectGUI/mainwindow.h
+++ PetProjectGUI/mainwindow.h
@@ -1,7 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <iostream>
 #include <QMainWindow>
+#include <QPixmap>
+#include <ctime>
+#include <QString>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -12,10 +17,12 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    void setWelcomePhoto();
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+    QPixmap welcomePic;
 };
 #endif // MAINWINDOW_H
