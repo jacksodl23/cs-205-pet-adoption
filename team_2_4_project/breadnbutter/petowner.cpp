aa03d553ddab5217b1181ec10357d305379b88d0
khaki <shyntask@lafayette.edu>
2021-04-07 17:06:09 -0400

    added insertion deletion methods and implemented accessor and mutator methods

--- team_2_4_project/breadnbutter/petowner.cpp
+++ team_2_4_project/breadnbutter/petowner.cpp
@@ -4,3 +4,72 @@ petOwner::petOwner()
 {
 
 }
+
+QString petOwner::getUsername()
+{
+    return username;
+}
+
+QString petOwner::getPassword()
+{
+    return password;
+}
+
+QString petOwner::getFirstName()
+{
+    return firstName;
+}
+
+QString petOwner::getLastName()
+{
+    return lastName;
+}
+
+QString petOwner::getEmail()
+{
+    return email;
+}
+
+void petOwner::setUsername(QString u)
+{
+    this->username = u;
+}
+
+void petOwner::setPassword(QString p)
+{
+    this->password = p;
+}
+
+void petOwner::setFirstName(QString fn)
+{
+    this->firstName = fn;
+}
+
+void petOwner::setLastName(QString ln)
+{
+    this->lastName = ln;
+}
+
+void petOwner::setEmail(QString e)
+{
+    this->email = e;
+}
+
+bool petOwner::insertIntoDB()
+{
+    QSqlQuery query;
+    query.prepare("insert into PetOwner (name)"
+                  "values (?)");
+    query.addBindValue(username);
+
+    return query.exec();
+}
+
+bool petOwner::deleteFromDB()
+{
+    QSqlQuery query;
+    query.prepare("delete from PetOwner where pet_id = ?");
+    query.addBindValue(petOwnerID);
+
+    return query.exec();
+}
--- team_2_4_project/breadnbutter/petowner.h
+++ team_2_4_project/breadnbutter/petowner.h
@@ -1,11 +1,38 @@
 #ifndef PETOWNER_H
 #define PETOWNER_H
-
+#include <QString>
+#include <QSql>
 
 class petOwner
 {
 public:
     petOwner();
+
+    // accessor methods
+    QString getUsername();
+    QString getPassword();
+    QString getFirstName();
+    QString getLastName();
+    QString getEmail();
+
+    // mutator methods
+    void setUsername(QString u);
+    void setPassword(QString p);
+    void setFirstName(QString fn);
+    void setLastName(QString ln);
+    void setEmail(QString e);
+
+    // database methods
+    bool insertIntoDB();
+    bool deleteFromDB();
+
+private:
+    int petOwnerID;
+    QString username;
+    QString password;
+    QString firstName;
+    QString lastName;
+    QString email;
 };
 
 #endif // PETOWNER_H
