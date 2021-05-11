2fdef46c7f50535ef84078abb58d0e823b7d50c9
Michael Vanderloo <mvanderloo@Michaels-MacBook-Air.local>
2021-05-10 23:25:28 -0400

    merge pet attributes to pet, added liked_by data

--- breadnbutter/database/drop_tables.sql
+++ breadnbutter/database/drop_tables.sql
@@ -1,4 +1,8 @@
-DROP TABLE if exists Adopter;
-DROP TABLE if exists Pet;
-DROP TABLE if exists Pet_Attributes;
+DROP TABLE if exists User;
 DROP TABLE if exists Shelter;
+DROP TABLE if exists Pet;
+DROP TABLE if exists Adopter_Preferences;
+DROP TABLE if exists Liked_By;
+DROP TABLE if exists Breed_Pref;
+DROP TABLE if exists Breeds;
+DROP TABLE if exists Location;
--- breadnbutter/database/populate_tables.sql
+++ breadnbutter/database/populate_tables.sql
@@ -1,21 +1,19 @@
 .mode csv
 
 DELETE from User;
-
-DELETE from Adopter_Preferences;
-
 DELETE from Shelter;
-
 DELETE from Pet;
-
-DELETE from Pet_Attributes;
+DELETE from Adopter_Preferences;
+DELETE from Liked_By;
+DELETE from Breed_Pref;
+DELETE from Breeds;
+DELETE from Location;
 
 .import ../../data/table_data/User_data.csv User
-
-.import ../../data/table_data/Adopter_Preferences_data.csv Adopter_Preferences
-
 .import ../../data/table_data/Shelter_data.csv Shelter
-
 .import ../../data/table_data/Pet_data.csv Pet
-
-.import ../../data/table_data/Pet_Attributes_data.csv Pet_Attributes
+.import ../../data/table_data/Adopter_Preferences_data.csv Adopter_Preferences
+.import ../../data/table_data/Liked_By_data.csv Liked_By
+--.import ../../data/table_data/Breed_Pref_data.csv Breed_Pref
+--.import ../../data/table_data/Breeds_data.csv Breeds
+--.import ../../data/table_data/Location_data.csv Location
