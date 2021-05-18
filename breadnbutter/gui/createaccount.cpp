2a0a0b301f64e158660195e5d9d3bce982b0913a
John Pitkin <pitkinj@lafayette.edu>
2021-05-17 20:42:08 -0400

    added some documentation

--- breadnbutter/gui/createaccount.cpp
+++ breadnbutter/gui/createaccount.cpp
@@ -57,12 +57,17 @@ void CreateAccount::on_roleBox_activated(const QString &arg1)
 }
 
 /*
+ * A PetOwner is a potential Adopter
+ *
  * Method to handle creating a PetOwner object based off of
  * the information provided in the CreateAccount page
  * If the new PetOwner information does not conflict with
  * another User that is already in the system then the
  * currentUser global variable is set to the new User and
  * the new PetOwner is added to the User configuration file
+ * Otherwise, the PetOwner is not added to the database or to
+ * the User configuration file of valid Users and an
+ * appropriate message pops up
  */
 void CreateAccount::signUpAdopter(QString firstName, QString lastName, QString location, QString email, QString phone, QString password)
 {
@@ -71,33 +76,63 @@ void CreateAccount::signUpAdopter(QString firstName, QString lastName, QString l
     PetOwner newAdopter(password, firstName, lastName, email, phone, location);
     signUpSuccessful = newAdopter.insertInDB();
 
+    // checking if the PetOwner was successfully added to the database
     if (signUpSuccessful) {
+        // assigning global var
         currentUser = newAdopter;
 
+        // adding PetOwner to configuration file that stores
+        // records of all Users added to the system
         writeUserToFile(newAdopter);
-    } else {
+    } else {	// PetOwner was not added to the database successfully
+        // checking to see if a given set of PetOwner information is already
+        // inside of the database
         if (newAdopter.existsInDB()) {
             QMessageBox::critical(this, "Email taken!", "This email is already taken. Please try again.");
         }
     }
 }
 
+
+/*
+ * Method to handle creating a ShelterOwner object based off of
+ * the information provided in the CreateAccount page
+ * If the new ShelterOwner information does not conflict with
+ * another User that is already in the system then the
+ * ShelterOwner is linked with the Shelter object, s, using a
+ * foriegn key, the currentUser global variable is set to the
+ * new ShelterOwner, and the new ShelterOwner is added to the
+ * User configuration file containing valid Users
+ * Otherwise, the new ShelterOwner is not added to the database
+ * or the configuration file and a message pops up
+ */
 void CreateAccount::linkShelterOwnerToShelter(Shelter s, QString location, QString firstName, QString email, QString phone, QString lastName, QString password)
 {
+    // creating ShelterOwner object with given information
     ShelterOwner newOwner(firstName, lastName, location, email, phone, password);
+    // checking if ShelterOwner info is already in database and that
+    // new information was successfully added to the database
     if (newOwner.insertInDB()) {
+        // linking the new ShelterOwner to the Shelter object, s with a
+        // foreign key
         QSqlQuery query;
         query.prepare("update shelter set owner_id = ? where shelter_id = ?");
         query.addBindValue(newOwner.getID());
         query.addBindValue(s.getShelterID());
 
+        // checking to make sure linking was successful
         signUpSuccessful = query.exec();
 
         if (signUpSuccessful) {
+            // if so, currentUser is assigned
             currentUser = newOwner;
 
+            // and the new ShelterOwner is added to the User
+            // configuration file containing all valid Users'
+            // information
             writeUserToFile(newOwner);
-        } else {
+        } else { // otherwise, the new ShelterOwner is not added to anything
+            // checking if this ShelterOwner is already in the database
             if (newOwner.existsInDB()) {
                 QMessageBox::critical(this, "Email taken!", "This email is already taken. Please try again.");
             }
@@ -105,8 +140,13 @@ void CreateAccount::linkShelterOwnerToShelter(Shelter s, QString location, QStri
     }
 }
 
+/*
+ * Method contains actions when the Ok/Sign up button
+ * is clicked on the CreateAccount page
+ */
 void CreateAccount::on_createOkay_clicked(QAbstractButton *button)
 {
+    // pulling information from all text fields
     QString firstName = ui->firstNameField->text();
     QString lastName = ui->lastNameField->text();
     QString email = ui->emailField->text();
@@ -115,22 +155,35 @@ void CreateAccount::on_createOkay_clicked(QAbstractButton *button)
     QString confirmPassword = ui->passwordField_2->text();
     QString location = ui->locationField->text();
 
+    // TODO
     if (button == ui->createOkay->button(QDialogButtonBox::Ok)) {
+        // checking to make sure that the password and confirm
+        // password fields match
         if (password != confirmPassword) {
             QMessageBox::critical(this, "Error!", "Passwords do not match - try again!");
             return;
         }
 
+        // checking to see if an Adopter is signing up
         if (ui->roleBox->currentIndex() == 0) {
+            // attempt to add adopter/PetOwner information
+            // to the database, User config file, and set
+            // the currentUser to the new adopter
             signUpAdopter(firstName, lastName, location, email, phone, password);
-        } else {
-            // Change to take in full name and shelter name values.
+        } else { // otherwise, a shelter/provider of pets is signing in
+            // Shelter object created with provided info
+            // lastName is used as the Shelter name whenever
+            // shelters are added to the system
             Shelter s(lastName, location, email);
+            // checking if the Shelter can be added to the
+            // database, and if so, adding it
             if (s.insertIntoDB()) {
 
                 QString oFirstName;
                 QString oLastName;
 
+                // splitting the firstName field into
+                // the actual first and last name of the ShelterOwner
                 QStringList pieces = firstName.split(" ");
                 for (int i = 0; i < pieces.size(); i++) {
                     if (i == 0)
@@ -139,13 +192,16 @@ void CreateAccount::on_createOkay_clicked(QAbstractButton *button)
                         oLastName = pieces.at(i);
                 }
 
+                // linking Shelter s to the new ShelterOwner made in the method
+                // using the information provided by the CreateAccount page
                 linkShelterOwnerToShelter(s, location, oFirstName, email, phone, oLastName, password);
-            } else {
+            } else { // error message if the Shelter could not be added to the database
                 QMessageBox::critical(this, "Unable to create shelter", "Something went wrong while trying to register a new shelter. Please try again.");
             }
         }
+        // closing the CreateAccount page
         this->close();
-    } else {
+    } else { // Ok/Sign up button not clicked
         qDebug() << "Should dismiss.";
         this->close();
     }
--- breadnbutter/gui/createaccount.h
+++ breadnbutter/gui/createaccount.h
@@ -21,22 +21,39 @@ class CreateAccount : public QDialog
     Q_OBJECT
 
 public:
+    // CreateAccount page constructor
     explicit CreateAccount(QWidget *parent = nullptr);
+    // CreateAccount page destructor
     ~CreateAccount();
 
+    // boolean for successful signup
     bool signUpSuccessful;
 
 private slots:
-    // void on_createOkay_accepted();
 
+    // slot method for handling the User's input data
+    // from the CreateAccount form when they click
+    // the Ok/Sign up button - creates necessary objects
+    // and adds to database and config files
     void on_createOkay_clicked(QAbstractButton *button);
 
+    // slot method to handle User input based off of
+    // their selected role - Adopter/PetOwner or
+    // Provider/ShelterOwner
     void on_roleBox_activated(const QString &arg1);
 
 private:
+    // private UI pointer
     Ui::CreateAccount *ui;
+
+    // private method for writing a User's information
+    // and encrypted ID to the User configuration file
+    // allowing for use of the application without
+    // repeated logging in
     void writeUserToFile(User newUser);
 
+    // takes all necessary adopter/PetOwner information and
+    //
     void signUpAdopter(QString firstName, QString lastName, QString location, QString email, QString phone, QString password);
     void linkShelterOwnerToShelter(Shelter s, QString location, QString firstName, QString email, QString phone, QString lastName, QString password);
 };
