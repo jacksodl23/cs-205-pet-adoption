ed5dc75303dbaabf607a87c61c0525fd744d4013
deja <jacksodl@lafayette.edu>
2021-05-18 19:56:34 -0400

    List of adopters that have liked pets of shelter properly hides unnecessary location info

--- breadnbutter/gui/shelteradopters.cpp
+++ breadnbutter/gui/shelteradopters.cpp
@@ -33,6 +33,7 @@ void ShelterAdopters::populateTable()
     query.prepare("select * from Liked_By "
                   "inner join User on user.user_id = liked_by.adopter_id "
                   "inner join Pet on pet.pet_id = liked_by.pet_id "
+                  "inner join Location on location.location_id = user.location_id "
                   "where pet.shelter_id = ?");
     query.addBindValue(shelter->getShelterID());
 
@@ -52,6 +53,7 @@ void ShelterAdopters::populateTable()
         ui->tableView->hideColumn(1); // adopter_id
         ui->tableView->hideColumn(2); // pet_id
         ui->tableView->hideColumn(3); // user_id
+        ui->tableView->hideColumn(5); // location_id
         ui->tableView->hideColumn(8); // password
         ui->tableView->hideColumn(9); // is_adopter
         ui->tableView->hideColumn(10); // pet_id
@@ -65,6 +67,9 @@ void ShelterAdopters::populateTable()
         ui->tableView->hideColumn(19); // origin
         ui->tableView->hideColumn(20); // hypoallergenic
         ui->tableView->hideColumn(21); // description
+        ui->tableView->hideColumn(22); // location_id
+        ui->tableView->hideColumn(26); // lattitude
+        ui->tableView->hideColumn(27); // longitude
     } else {
         qDebug() << "Error getting likes:" << query.lastError().text();
     }
