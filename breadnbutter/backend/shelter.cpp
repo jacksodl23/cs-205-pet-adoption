3f63b44535fdd9d26be36b066d81db17d1b19878
MVanderloo <vanderlm@lafayette.edu>
2021-05-18 13:13:11 -0400

    implemented distance

--- breadnbutter/backend/shelter.cpp
+++ breadnbutter/backend/shelter.cpp
@@ -175,3 +175,16 @@ void Shelter::setShelterID(int value)
 {
     shelterID = value;
 }
+
+double distance(double lat1, double long1, double lat2, double long2) {
+	// Haversine Formula
+	const double pi = 3.1415926535;
+	const double R = 3958.8; // radius of Earth (mi)
+	lat1 *= 0.0174533;  // to radians
+	long1 *= 0.0174533; // to radians
+	lat2 *= 0.0174533;  // to radians
+	long2 *= 0.0174533; // to radians
+
+    double a = std::pow(std::sin((lat2-lat1)/2), 2) + std::cos(lat1)*std::cos(lat2)*std::pow(std::sin((long2-long1)/2), 2);
+    return 2 * R * std::atan2(std::sqrt(a), std::sqrt(1-a));
+}
